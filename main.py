f5919c10852f500328c4ada0252d0e10219944b9
pandyasmit-08 <smitbpandya@gmail.com>
2025-05-15 00:46:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,4 +31,5 @@ def predict(news: NewsItem):
         return {"error": "Internal server error", "details": str(e)}
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))  # use PORT env var if set, else default to 8000
-    uvicorn.run("2:app", host="0.0.0.0", port=port)
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
